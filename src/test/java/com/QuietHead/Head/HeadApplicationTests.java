b218cf7ba3868f259905305b743e9400d637e835
FELPSTS <macleimotog0@gmail.com>
2025-04-16 11:51:57 -0300

    first commit

---
+++ src/test/java/com/QuietHead/Head/HeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuietHead.Head;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
