f37def145095d5ecfbf24e977773dc39a4a2972d
Felipe Torquato <112289573+FELPSTS@users.noreply.github.com>
2025-06-09 20:19:01 -0300

    Update EventService.java

--- src/main/java/com/QuietHead/Head/service/EventService.java
+++ src/main/java/com/QuietHead/Head/service/EventService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.QuietHead.Head.domain.Event;
 import com.QuietHead.Head.repository.EventRepository;
-import jakarta.annotation.PostConstruct;
 
 import java.util.List;
 import java.util.Optional;
